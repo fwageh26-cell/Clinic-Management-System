56fa30d83c313dd41e24878225046729960cb07d
fwageh26-cell <fwageh26@gmail.com>
2026-02-12 21:58:57 +0200

    Update patients.js

--- api/patients.js
+++ api/patients.js
@@ -4,22 +4,18 @@ const uri = process.env.MONGODB_URI;
 let client;
 
 module.exports = async (req, res) => {
-    try {
-        if (!client) {
-            client = new MongoClient(uri);
-            await client.connect();
-        }
-        const db = client.db('clinic_db');
-        const collection = db.collection('patients');
+    if (!client) {
+        client = new MongoClient(uri);
+        await client.connect();
+    }
+    const db = client.db('clinic_db');
+    const collection = db.collection('patients');
 
-        if (req.method === 'POST') {
-            await collection.insertOne(req.body);
-            return res.status(200).json({ success: true });
-        } else {
-            const data = await collection.find({}).toArray();
-            return res.status(200).json(data);
-        }
-    } catch (error) {
-        return res.status(500).json({ error: error.message });
+    if (req.method === 'POST') {
+        await collection.insertOne(req.body);
+        return res.status(200).json({ success: true });
+    } else {
+        const data = await collection.find({}).toArray();
+        return res.status(200).json(data);
     }
 };
